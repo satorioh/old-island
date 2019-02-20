78ae8126ed2de154dd1bb3e68fec4ce138689d32
Roubin <wangbinxp@sina.com>
2019-02-20 16:37:41 +0800

    music component

--- components/classic/music/music.js
+++ components/classic/music/music.js
@@ -1,26 +1,44 @@
 // components/classic/music/music.js
 import {classicBeh} from "../classic-beh";
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
   behaviors:[classicBeh],
   properties: {
-    src:String
+    src:String,
+    title:String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: 'images/player@waitting.png',
-    playSrc: 'images/player@playing.png'
+    playing:false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay (e) {
+      this.setData({
+        playing: !this.data.playing,
+      })
+      if (this.data.playing) {
+        if(mMgr.src == this.properties.src){
+          mMgr.play()
+        } else {
+          mMgr.src = this.properties.src
+        }
+        mMgr.title = this.properties.title
+      } else {
+        mMgr.pause()
+      }
+    },
   }
 })
